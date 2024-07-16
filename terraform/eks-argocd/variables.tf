5329859aef83c8c1aa2b69d2a3d3171a213c792b
BENMALEKarim <fk_benmalek@esi.dz>
2024-07-16 11:22:14 +0200

    delete annotation aws-load-balancer-controller-appset

--- terraform/eks-argocd/variables.tf
+++ terraform/eks-argocd/variables.tf
@@ -36,9 +36,9 @@ variable "addons" {
     enable_cert_manager                 = false
     enable_aws_ebs_csi_resources        = false # generate gp2 and gp3 storage classes for ebs-csi
     enable_aws_cloudwatch_metrics       = false
-    enable_external_secrets             = true
+    enable_external_secrets             = false
     enable_aws_load_balancer_controller = true
-    enable_aws_for_fluentbit            = true
+    enable_aws_for_fluentbit            = false
     enable_karpenter                    = false
     enable_aws_ingress_nginx            = true # inginx configured with AWS NLB
     # oss
