6ef805fc84fe28e1e097ecb7eb4557e1210ff875
BENMALEKarim <fk_benmalek@esi.dz>
2024-07-16 11:18:57 +0200

    add pod-identity to eks

--- terraform/eks-argocd/main.tf
+++ terraform/eks-argocd/main.tf
@@ -2,13 +2,18 @@
 # EKS Blueprints Addons
 ################################################################################
 module "eks_blueprints_addons" {
-  source  = "aws-ia/eks-blueprints-addons/aws"
-  version = "~> 1.0"
+  #source  = "aws-ia/eks-blueprints-addons/aws"
+  #version = "~> 1.0"
+  source = "github.com/aws-ia/terraform-aws-eks-blueprints-addon//?ref=pod-identity"
 
   cluster_name      = module.eks.cluster_name
   cluster_endpoint  = module.eks.cluster_endpoint
   cluster_version   = module.eks.cluster_version
   oidc_provider_arn = module.eks.oidc_provider_arn
+
+  # Using pod-identity
+  enable_pod_identity             = true
+  create_pod_identity_association = true
 
   # Using GitOps Bridge (Skip Helm Install in Terraform)
   create_kubernetes_resources = false
